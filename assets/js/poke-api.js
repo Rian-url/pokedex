2b88fd941d05b0ac95e559839257fc7edb0e84d0
Rian-usr <rian.jesus5@etec.sp.gov.br>
2023-11-08 22:01:04 -0300

    atualizando formatação

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,9 +1,18 @@
 const pokeApi = {}
 
+pokeApi.getPokemonsDetail = (pokemon) =>{
+    return fetch(pokemon.url).then((response) => response.json())
+}
+
 pokeApi.getPokemons = (offset , limit ) =>{
-    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
     return fetch(url)
    .then( (response) => response.json()) 
    .then( (jsonBody) => jsonBody.results)
+   .then((pokemons) => pokemons.map(pokeApi.getPokemonsDetail))
+   .then((detailRequests) => Promise.all(detailRequests))
+   .then((pokemonDetails) => pokemonDetails )
+   
    .catch((error) => console.error(error))
 }
